5135e74be98830941a11cf812c7bf300ca3ff35f
itheima <itheima@itcast.cn>
2025-09-22 21:27:42 +0800

    子应用的urls

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -7,4 +7,6 @@ from django.shortcuts import render
 # 1.视图函数的第一个参数是接收请求，这个请求其实就是HttpRequest的类对象
 # 2.必须返回一个响应
 def index(request):
-    return HttpResponse("ok")
+
+    return HttpResponse("hello python3")
+
--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #path(路由，视图函数名)
+    #第一种写法
+    # path('index/',index),
+
+    #第二种写法，更普遍使用
+    path('', include('book.urls'))
 ]
+
