5a2890a84d2736c8f7a52a26e25f35b7509d2da2
itheima <itheima@itcast.cn>
2025-09-24 10:09:45 +0800

    数据库改为mysql

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -99,10 +99,20 @@ WSGI_APPLICATION = 'bookmanager.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+    # 改为mysql模式数据库
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST':'localhost',
+            'POST':3306,
+            'USER':'root',
+            'PASSWORD':'246859',
+            'NAME': 'book_base', #数据库名字
+        }
 }
 
 
